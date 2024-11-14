a2a1d9331ab572a7566c1483640541fb5f5ca67f
westernat <westernat233@163.com>
2024-11-14 17:27:00 +0800

    半砖型棕榈树叶

--- ConfluenceOfTheAfterlife/src/main/java/org/confluence/mod/common/block/natural/PalmLeaves.java
+++ ConfluenceOfTheAfterlife/src/main/java/org/confluence/mod/common/block/natural/PalmLeaves.java
@@ -1,33 +1,114 @@
 package org.confluence.mod.common.block.natural;
 
 import net.minecraft.core.BlockPos;
+import net.minecraft.core.Direction;
 import net.minecraft.server.level.ServerLevel;
+import net.minecraft.tags.FluidTags;
 import net.minecraft.util.RandomSource;
+import net.minecraft.world.entity.player.Player;
+import net.minecraft.world.item.ItemStack;
 import net.minecraft.world.item.context.BlockPlaceContext;
+import net.minecraft.world.level.BlockGetter;
 import net.minecraft.world.level.LevelAccessor;
 import net.minecraft.world.level.block.Block;
 import net.minecraft.world.level.block.LeavesBlock;
 import net.minecraft.world.level.block.state.BlockState;
+import net.minecraft.world.level.block.state.StateDefinition;
+import net.minecraft.world.level.block.state.properties.BlockStateProperties;
+import net.minecraft.world.level.block.state.properties.EnumProperty;
+import net.minecraft.world.level.block.state.properties.SlabType;
+import net.minecraft.world.level.material.Fluid;
 import net.minecraft.world.level.material.FluidState;
 import net.minecraft.world.level.material.Fluids;
+import net.minecraft.world.level.pathfinder.PathComputationType;
+import net.minecraft.world.phys.shapes.CollisionContext;
+import net.minecraft.world.phys.shapes.Shapes;
+import net.minecraft.world.phys.shapes.VoxelShape;
+import org.jetbrains.annotations.NotNull;
+
+import javax.annotation.Nullable;
 
 public class PalmLeaves extends LeavesBlock {
+    public static final EnumProperty<SlabType> TYPE = BlockStateProperties.SLAB_TYPE;
+    protected static final VoxelShape BOTTOM_AABB = box(0.0, 0.0, 0.0, 16.0, 8.0, 16.0);
+    protected static final VoxelShape TOP_AABB = box(0.0, 8.0, 0.0, 16.0, 16.0, 16.0);
+
     public PalmLeaves(Properties properties) {
         super(properties);
+        registerDefaultState(stateDefinition.any()
+                .setValue(DISTANCE, 7)
+                .setValue(PERSISTENT, false)
+                .setValue(WATERLOGGED, false)
+                .setValue(TYPE, SlabType.BOTTOM));
     }
 
     @Override
-    protected void tick(BlockState state, ServerLevel level, BlockPos pos, RandomSource random) {
+    protected void createBlockStateDefinition(StateDefinition.Builder<Block, BlockState> builder) {
+        builder.add(DISTANCE, PERSISTENT, WATERLOGGED, TYPE);
+    }
+
+    @Override
+    protected @NotNull VoxelShape getShape(BlockState state, @NotNull BlockGetter level, @NotNull BlockPos pos, @NotNull CollisionContext context) {
+        return switch (state.getValue(TYPE)) {
+            case DOUBLE -> Shapes.block();
+            case TOP -> TOP_AABB;
+            default -> BOTTOM_AABB;
+        };
+    }
+
+    @Override
+    protected void tick(@NotNull BlockState state, ServerLevel level, @NotNull BlockPos pos, @NotNull RandomSource random) {
         level.setBlock(pos, updateDistance(state, level, pos), Block.UPDATE_ALL);
     }
 
     @Override
     public BlockState getStateForPlacement(BlockPlaceContext context) {
-        FluidState fluidstate = context.getLevel().getFluidState(context.getClickedPos());
-        BlockState blockstate = defaultBlockState()
-                .setValue(PERSISTENT, true)
-                .setValue(WATERLOGGED, fluidstate.getType() == Fluids.WATER);
-        return updateDistance(blockstate, context.getLevel(), context.getClickedPos());
+        BlockPos blockPos = context.getClickedPos();
+        FluidState fluidState = context.getLevel().getFluidState(blockPos);
+
+        BlockState ret = context.getLevel().getBlockState(blockPos);
+        if (ret.is(this)) {
+            ret = ret.setValue(TYPE, SlabType.DOUBLE).setValue(WATERLOGGED, false);
+        } else {
+            BlockState blockState = defaultBlockState().setValue(TYPE, SlabType.BOTTOM).setValue(WATERLOGGED, fluidState.getType() == Fluids.WATER);
+            Direction direction = context.getClickedFace();
+            ret = direction != Direction.DOWN && (direction == Direction.UP || !(context.getClickLocation().y - (double) blockPos.getY() > 0.5))
+                    ? blockState : blockState.setValue(TYPE, SlabType.TOP);
+        }
+
+        return updateDistance(ret.setValue(PERSISTENT, true), context.getLevel(), context.getClickedPos());
+    }
+
+    @Override
+    protected boolean canBeReplaced(BlockState state, BlockPlaceContext useContext) {
+        ItemStack itemstack = useContext.getItemInHand();
+        SlabType slabtype = state.getValue(TYPE);
+        if (slabtype == SlabType.DOUBLE || !itemstack.is(asItem())) {
+            return false;
+        } else if (useContext.replacingClickedOnBlock()) {
+            boolean flag = useContext.getClickLocation().y - (double) useContext.getClickedPos().getY() > 0.5;
+            Direction direction = useContext.getClickedFace();
+            return slabtype == SlabType.BOTTOM
+                    ? direction == Direction.UP || flag && direction.getAxis().isHorizontal()
+                    : direction == Direction.DOWN || !flag && direction.getAxis().isHorizontal();
+        } else {
+            return true;
+        }
+    }
+
+    @Override
+    public boolean placeLiquid(@NotNull LevelAccessor level, @NotNull BlockPos pos, BlockState state, @NotNull FluidState fluidState) {
+        return state.getValue(TYPE) != SlabType.DOUBLE && super.placeLiquid(level, pos, state, fluidState);
+    }
+
+    @Override
+    public boolean canPlaceLiquid(@Nullable Player player, @NotNull BlockGetter level, @NotNull BlockPos pos, BlockState state, @NotNull Fluid fluid) {
+        return state.getValue(TYPE) != SlabType.DOUBLE && super.canPlaceLiquid(player, level, pos, state, fluid);
+    }
+
+    @Override
+    protected boolean isPathfindable(@NotNull BlockState state, @NotNull PathComputationType pathComputationType) {
+        return pathComputationType == PathComputationType.WATER && state.getFluidState().is(FluidTags.WATER);
     }
 
     private static BlockState updateDistance(BlockState state, LevelAccessor level, BlockPos pos) {
