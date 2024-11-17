8fc08eec15855b904b26af2a35c89cc9fe4e2fa4
westernat <westernat233@163.com>
2024-11-17 17:01:38 +0800

    补三个英文

---
+++ ConfluenceOfTheAfterlife/src/main/java/org/confluence/mod/common/entity/minecart/MechanicalCartEntity.java
@@ -0,0 +1,14 @@
+package org.confluence.mod.common.entity.minecart;
+
+import net.minecraft.world.entity.EntityType;
+import net.minecraft.world.level.Level;
+
+public class MechanicalCartEntity extends BaseMinecartEntity{
+    public MechanicalCartEntity(EntityType<? extends BaseMinecartEntity> entityType, Level level) {
+        super(entityType, level);
+    }
+
+    public MechanicalCartEntity(Level level, double x, double y, double z, Abilities<MechanicalCartEntity> abilities) {
+        super(level, x, y, z, abilities);
+    }
+}
