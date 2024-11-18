b824b6366a950fbb059cfdc222aa7bc2f389fdaa
westernat <westernat233@163.com>
2024-11-18 22:55:14 +0800

    存星象数据

--- ConfluenceOfTheAfterlife/src/main/java/org/confluence/mod/common/data/saved/ConfluenceData.java
+++ ConfluenceOfTheAfterlife/src/main/java/org/confluence/mod/common/data/saved/ConfluenceData.java
@@ -2,28 +2,42 @@ package org.confluence.mod.common.data.saved;
 
 import net.minecraft.core.HolderLookup;
 import net.minecraft.nbt.CompoundTag;
+import net.minecraft.nbt.ListTag;
+import net.minecraft.nbt.Tag;
 import net.minecraft.server.level.ServerLevel;
+import net.minecraft.util.Tuple;
 import net.minecraft.world.level.saveddata.SavedData;
 import net.neoforged.neoforge.network.PacketDistributor;
 import org.confluence.mod.network.s2c.GamePhasePacketS2C;
 import org.confluence.terra_curio.network.s2c.WindSpeedPacketS2C;
 import org.jetbrains.annotations.NotNull;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class ConfluenceData extends SavedData {
     private GamePhase gamePhase;
     private float windSpeedX;
     private float windSpeedZ;
+    private final List<Tuple<Float, Float>> starPhases;
 
     ConfluenceData() {
         this.gamePhase = GamePhase.BEFORE_SKELETRON;
         this.windSpeedX = 0.0F;
         this.windSpeedZ = 0.0F;
+        this.starPhases = new ArrayList<>();
     }
 
     ConfluenceData(CompoundTag nbt, HolderLookup.@NotNull Provider registries) {
         this.gamePhase = GamePhase.getById(nbt.getInt("gamePhase"));
         this.windSpeedX = nbt.getFloat("windSpeedX");
         this.windSpeedZ = nbt.getFloat("windSpeedZ");
+        ListTag listTag = nbt.getList("starPhases", Tag.TAG_COMPOUND);
+        this.starPhases = new ArrayList<>();
+        for (Tag tag : listTag) {
+            CompoundTag phase = (CompoundTag) tag;
+            starPhases.add(new Tuple<>(phase.getFloat("radius"), phase.getFloat("angle")));
+        }
     }
 
     public static ConfluenceData get(ServerLevel serverLevel) {
@@ -35,6 +49,14 @@ public class ConfluenceData extends SavedData {
         nbt.putInt("gamePhase", gamePhase.ordinal());
         nbt.putFloat("windSpeedX", windSpeedX);
         nbt.putFloat("windSpeedZ", windSpeedZ);
+        ListTag listTag = new ListTag();
+        for (Tuple<Float, Float> phase : starPhases) {
+            CompoundTag tag = new CompoundTag();
+            tag.putFloat("radius", phase.getA());
+            tag.putFloat("angle", phase.getB());
+            listTag.add(tag);
+        }
+        nbt.put("starPhases", listTag);
         return nbt;
     }
 
@@ -69,5 +91,9 @@ public class ConfluenceData extends SavedData {
 
     public float getWindSpeedZ() {
         return windSpeedZ;
+    }
+
+    public List<Tuple<Float, Float>> getStarPhases() {
+        return starPhases;
     }
 }
