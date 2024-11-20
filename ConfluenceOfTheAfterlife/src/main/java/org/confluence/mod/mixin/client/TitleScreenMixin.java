74ea1b04e17cff674fabfb5fd75461e0b1cdc910
westernat <westernat233@163.com>
2024-11-20 23:12:52 +0800

    替换为静态常量

--- ConfluenceOfTheAfterlife/src/main/java/org/confluence/mod/mixin/client/TitleScreenMixin.java
+++ ConfluenceOfTheAfterlife/src/main/java/org/confluence/mod/mixin/client/TitleScreenMixin.java
@@ -4,27 +4,35 @@ import net.minecraft.client.gui.GuiGraphics;
 import net.minecraft.client.gui.screens.Screen;
 import net.minecraft.client.gui.screens.TitleScreen;
 import net.minecraft.network.chat.Component;
+import net.minecraft.network.chat.MutableComponent;
 import net.minecraft.resources.ResourceLocation;
 import org.confluence.mod.Confluence;
 import org.confluence.mod.client.gui.screens.MainWikiScreen;
 import org.confluence.mod.util.GuiUtils;
 import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Unique;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
 @Mixin(TitleScreen.class)
 public abstract class TitleScreenMixin extends Screen {
+    @Unique
+    private static final ResourceLocation confluence$button = Confluence.asResource("textures/item/sword/copper_short_sword.png");
+    @Unique
+    private static final MutableComponent confluence$title = Component.translatable("title.confluence.wiki");
+
     protected TitleScreenMixin(Component title) {
         super(title);
     }
 
     @Inject(method = "render", at = @At("RETURN"))
     private void render(GuiGraphics guiGraphics, int mouseX, int mouseY, float partialTick, CallbackInfo ci) {
-        int l = this.height / 4 + 32;
-        this.addRenderableWidget(GuiUtils.buildIconButton(guiGraphics, ResourceLocation.fromNamespaceAndPath(Confluence.MODID,
-                        "textures/item/sword/copper_short_sword.png"), this.width / 2 - 124, l + 72,
-                20, 20, 16, 16, (p) -> minecraft.setScreen(new MainWikiScreen(true)),
-                Component.translatable("title.confluence.wiki")));
+        int l = height / 4 + 32;
+        addRenderableWidget(GuiUtils.buildIconButton(
+                guiGraphics, confluence$button, width / 2 - 124, l + 72, 20, 20, 16, 16,
+                button -> minecraft.setScreen(new MainWikiScreen(true)),
+                confluence$title
+        ));
     }
 }
