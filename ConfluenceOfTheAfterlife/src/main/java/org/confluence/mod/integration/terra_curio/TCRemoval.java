f7d1ced2a7c57ac70e520316655a2d3178c6854a
MagicHarp <@2749380016@qq.com>
2024-11-25 22:45:45 +0800

    在本体中取消一些泰拉饰品独有的合成

--- ConfluenceOfTheAfterlife/src/main/java/org/confluence/mod/integration/terra_curio/TCRemoval.java
+++ ConfluenceOfTheAfterlife/src/main/java/org/confluence/mod/integration/terra_curio/TCRemoval.java
@@ -10,5 +10,10 @@ import java.util.List;
 public final class TCRemoval {
     public static final List<ResourceLocation> RECIPES = Util.make(new ArrayList<>(), list -> {
         list.add(TerraCurio.asResource("blizzard_in_a_bottle"));
+        list.add(TerraCurio.asResource("workshop"));
+        list.add(TerraCurio.asResource("flurry_boots"));
+        list.add(TerraCurio.asResource("ice_skates"));
+        list.add(TerraCurio.asResource("rocket_boots"));
+        list.add(TerraCurio.asResource("stopwatch"));
     });
 }
